d249557afe2f19145a860985d8d7a31a2244c1dc
Toskgreg <toskingregory@icloud.com>
2018-06-29 16:40:35 -0700

    restructured OOP of API

--- app/__init__.py
+++ app/__init__.py
@@ -1,83 +1,17 @@
-# app/__init__.py
+from flask import Flask, redirect
+from app.views import rides
+from flasgger import Swagger
 
+app = Flask(__name__)
 
-from flask_api import FlaskAPI
+template = {
+    "swagger": 2.0,
+    "version": "v1",
+}
 
+Swagger(app, template=template)
 
-# local import
-from instance.config import app_config
-
-
-from app.models import Rides
-from flask import request, jsonify, abort
-
-def create_app(config_name):
-    app = FlaskAPI(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
-    
-    
-
-
-    @app.route('/api/v1/ride/', methods=['POST', 'GET'])
-    def rides():
-        if request.method == "POST":
-            start_address = str(request.data.get('Start Address', ''))
-            destination = str(request.data.get('Destination', ''))
-            if start_address and destination:
-                ride = Rides(start_address,destination,)
-                
-                
-                response = jsonify({
-                    'id': ride.id,
-                    'start_address': ride.start_address,
-                    'destination': ride.destination,
-                    
-                })
-                response.status_code = 201
-                return response
-        
-            
-
-    
-    @app.route('/api/v1/ride/<int:id>', methods=['GET', 'PUT', 'DELETE'])
-    def ride_list_manipulation(id, **kwargs):
-     # retrieve a ride using it's ID
-        ride = Rides(id,destination="mutungo")
-        if not ride:
-            # Raise an HTTPException with a 404 not found status code
-            abort(404)
-
-        if request.method == 'DELETE':
-            
-            return {
-            "message": "ride {} deleted successfully".format(ride.id) 
-         }, 200
-
-        elif request.method == 'PUT':
-            
-            start_address = str(request.data.get('Start Address', ''))
-            destination = str(request.data.get('Destination', ''))
-            ride.start_address = start_address
-            ride.destination =destination
-            
-            response = jsonify({
-                'id': ride.id,
-                'start_address': ride.start_address,
-                'destination': ride.destination,
-                
-            })
-            response.status_code = 200
-            return response
-        else:
-            # GET
-            response = jsonify({
-                'id': ride.id,
-                'start_address': ride.start_address,
-                'destination': ride.destination,
-               
-            })
-            response.status_code = 200
-            return response
-
-    return app
+@app.route('/')
+def index():
+    return redirect('/')
+app.register_blueprint(rides)
--- app/models.py
+++ app/models.py
@@ -1,14 +1,90 @@
 # app/models.py
 
-from flask import abort
+#variable that holds all the journeys and their details
+rides = []
+requestr =[]
 
 
-class Rides(object):
-    """This class represents the rides db."""
-    def __init__(self,  start_address, destination):
-        """intiate class db variables"""
-        self.id = 0
+
+class Ride:
+
+    def __init__(self, destination, start_address): 
+
+    #initialising class attributes
+        self.ride_id = self.auto_id(rides)
         self.start_address = start_address
         self.destination = destination
-        self.rides = []    
         
+
+    def addOne(self):
+   
+    #creating a dictionary from an object and returning it
+
+        ride = {
+            "Id": self.ride_id,
+            "start_address": self.start_address,
+            "destination": self.destination,
+           
+        }
+        rides.append(ride)
+
+        return ride
+    @staticmethod
+    def auto_id(ride_ids):
+    #methods aids auto generation of ride_ids
+        if ride_ids:
+            return ride_ids[-1].get("Id") + 1
+        return 1
+
+    @staticmethod
+    def returnAll():
+    #This method retrieves or returns all rides in the dictionary
+        
+        if rides:
+            return rides
+        return "No existing Ride offer"
+
+    @staticmethod
+    def returnOne( ride_id):
+    #method returns a single ride
+        for ride in rides:
+            if ride.get('Id') == ride_id:
+                return ride
+            continue
+
+        return "Ride not Found"
+
+    @staticmethod
+    def ride_request( ride_id):
+    #method returns a single ride
+        for ride in rides:
+            if ride.get('Id') == ride_id:
+                return ride
+            continue
+
+        return "Ride not Found"
+
+    
+    @staticmethod
+    def getride( ride_id):
+    #method returns a single ride
+        for ride in rides:
+            if ride.get('Id') == ride_id:
+                return ride
+            continue
+
+        return "Ride not Found"
+    
+
+     
+
+
+    @staticmethod
+    def removeOne( ride_id):
+    #method deletes a specified ride
+        for count, ride in enumerate(rides):
+            if ride.get("Id") == ride_id:
+                rides.pop(count)
+                return rides
+        return "Ride not Found"
+
